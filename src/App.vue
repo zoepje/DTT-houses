990ad350dab253465940ff628abc87c49844252f
zoepje <zoe.penner@hva.nl>
2024-12-07 15:18:54 +0100

    move header bottom of the screen on mobile

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,19 @@
 </template>
 
 <style scoped>
-  header{
-    position: sticky;
-    top: 0;
+  header {
+    position: fixed;
+    bottom: 0;
   }
 
   main {
     height: 100vh;
+  }
+
+  @media only screen and (min-width: 600px){
+    header {
+      position: sticky;
+      top: 0;
+    }
   }
 </style>
